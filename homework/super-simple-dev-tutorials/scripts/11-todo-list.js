9e68de3e58219fdf3f54fbb079586070671ef157
rc.devs9 <rc.devs9@gmail.com>
2025-01-30 11:52:18 -0600

    not sure, just refreshing

--- homework/super-simple-dev-tutorials/scripts/11-todo-list.js
+++ homework/super-simple-dev-tutorials/scripts/11-todo-list.js
@@ -40,8 +40,7 @@ function addTodo() {
 function renderTodoList() {
   //Accumulator Pattern
   let todoListHTML = '';//(accumulator variable)
-
- 
+  
   for (let i = 0; i < todoList.length; i++) {
     const todo = todoList[i];
     const html = `<p>${todo}</p>`; 
