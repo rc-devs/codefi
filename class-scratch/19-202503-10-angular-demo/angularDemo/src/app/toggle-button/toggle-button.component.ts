b060b224ebe8680f65d8d0db2c6b2484648faf65
rc-devs <rc.devs9@gmail.com>
2025-03-10 14:16:19 -0500

    created toggle button component

---
+++ class-scratch/19-202503-10-angular-demo/angularDemo/src/app/toggle-button/toggle-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-button',
+  imports: [],
+  templateUrl: './toggle-button.component.html',
+  styleUrl: './toggle-button.component.css'
+})
+export class ToggleButtonComponent {
+
+}
