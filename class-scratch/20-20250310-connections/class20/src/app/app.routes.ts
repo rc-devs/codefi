137e1135daadecc97c1b2f5e0c0dca40c0dd010d
rc-devs <rc.devs9@gmail.com>
2025-03-10 12:32:18 -0500

    create quote-rotator component, import in app.routes

--- class-scratch/20-20250310-connections/class20/src/app/app.routes.ts
+++ class-scratch/20-20250310-connections/class20/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { QuoteRotatorComponent } from './quote-rotator/quote-rotator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: QuoteRotatorComponent
+  }
+];
---
+++ class-scratch/20-20250310-connections/class20/src/app/quote-rotator/quote-rotator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-rotator',
+  imports: [],
+  templateUrl: './quote-rotator.component.html',
+  styleUrl: './quote-rotator.component.css'
+})
+export class QuoteRotatorComponent {
+
+}
