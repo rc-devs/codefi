1f714b4e8f2badab0edfaa89e07ab8439ba97a7c
rc-devs <rc.devs9@gmail.com>
2025-03-10 14:42:42 -0500

    input with dynamic text display

--- class-scratch/19-202503-10-angular-demo/angularDemo/src/app/app.component.ts
+++ class-scratch/19-202503-10-angular-demo/angularDemo/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header-component/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ToggleButtonComponent } from './toggle-button/toggle-button.component';
+import { InputComponent } from './input/input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ToggleButtonComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ToggleButtonComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- class-scratch/19-202503-10-angular-demo/angularDemo/src/app/input/input.component.ts
+++ class-scratch/19-202503-10-angular-demo/angularDemo/src/app/input/input.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
-
+  typedText = '';
 }
