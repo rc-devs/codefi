736f8c6ab3d92f3a4f3e615bbc7a9874d19bbb33
rc-devs <rc.devs9@gmail.com>
2025-03-13 13:01:30 -0500

    idk

--- class-scratch/20-20250310-connections/class20/src/app/quote-rotator/quote-rotator.component.ts
+++ class-scratch/20-20250310-connections/class20/src/app/quote-rotator/quote-rotator.component.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { QuoteCardComponent } from './quote-card/quote-card.component';
 
 @Component({
   selector: 'app-quote-rotator',
-  imports: [QuoteCardComponent],
+  imports: [],
   templateUrl: './quote-rotator.component.html',
   styleUrl: './quote-rotator.component.css'
 })
 export class QuoteRotatorComponent {
     quotes = signal(['quote 1', 'quote 2', 'quote 3']);
-    currentQuote = signal(this.quotes);
+    currentQuote = signal(this.quotes()[0]);
+
+    nextQuote(){
+      const currentIndex = this.quotes().indexOf(this.currentQuote());
+      const nextIndex = (currentIndex + 1) % this.quotes().length;
+      this.currentQuote.set(this.quotes()[nextIndex])
+    }
 }
