d2cca2f35dbf6a1a5a8be5083f3a49d31ba36c86
rc-devs <rc.devs9@gmail.com>
2025-03-13 14:06:25 -0500

    connected fav color to app component, rplace code from task list rip

--- homework/20-angular-signals/20-homework/src/app/app.component.ts
+++ homework/20-angular-signals/20-homework/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from "./counter/counter.component";
 import { TaskListComponent } from './task-list/task-list.component';
+import { FavColorComponent } from './fav-color/fav-color.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent, TaskListComponent],
+  imports: [RouterOutlet, CounterComponent, TaskListComponent, FavColorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- homework/20-angular-signals/20-homework/src/app/fav-color/fav-color.component.ts
+++ homework/20-angular-signals/20-homework/src/app/fav-color/fav-color.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fav-color',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './fav-color.component.html',
   styleUrl: './fav-color.component.css'
 })
 export class FavColorComponent {
+  colors = signal( ['Red', 'Green', 'Blue'])
+  newColor = '';
+
+  updateList(){
+    this.colors.update((colors)=> [...colors, this.newColor])
+    this.newColor = '';
+  }
+
 
 }
