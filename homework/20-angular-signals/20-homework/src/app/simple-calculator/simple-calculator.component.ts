505b2d9ef10b4f2014982e0531aa6c5f311eeedd
rc-devs <rc.devs9@gmail.com>
2025-03-13 15:21:29 -0500

    simple calculator go unto hell

--- homework/20-angular-signals/20-homework/src/app/simple-calculator/simple-calculator.component.ts
+++ homework/20-angular-signals/20-homework/src/app/simple-calculator/simple-calculator.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-simple-calculator',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './simple-calculator.component.html',
   styleUrl: './simple-calculator.component.css'
 })
 export class SimpleCalculatorComponent {
+  input1:number = 0;
+  input2:number = 0;
+  answer = 0;
 
+  //i know this is fucky
+  onSubract(){
+    this.answer = this.input1 - this.input2;
+  };
+  onAdd(){
+    this.answer = Number(this.input1) + Number(this.input2);
+  };
+  onMultiply(){
+    this.answer = this.input1 * this.input2;
+  };
+  onDivide(){
+    this.answer = this.input1 / this.input2;
+  }
 }
