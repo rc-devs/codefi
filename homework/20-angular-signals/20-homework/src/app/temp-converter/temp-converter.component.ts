7e68e54d1fd127c190debf8939736752c3415231
rc-devs <rc.devs9@gmail.com>
2025-03-13 14:37:14 -0500

    ts for number in celcius, spelling corrections

--- homework/20-angular-signals/20-homework/src/app/temp-converter/temp-converter.component.ts
+++ homework/20-angular-signals/20-homework/src/app/temp-converter/temp-converter.component.ts
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './temp-converter.component.css'
 })
 export class TempConverterComponent {
-  celcius = 0;
+  celcius: number = 0;
   fahrenheit: number = 0
 
   onConvert(){
