11a7e2d24b134714d831c07ad6b57c6fdb1e71b5
rc-devs <rc.devs9@gmail.com>
2025-03-13 14:01:18 -0500

    create favcolor component

---
+++ homework/20-angular-signals/20-homework/src/app/fav-color/fav-color.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fav-color',
+  imports: [],
+  templateUrl: './fav-color.component.html',
+  styleUrl: './fav-color.component.css'
+})
+export class FavColorComponent {
+
+}
