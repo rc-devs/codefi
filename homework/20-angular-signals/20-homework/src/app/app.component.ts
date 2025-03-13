4f520cd4c283e316007efa4512f4a338c870f476
rc-devs <rc.devs9@gmail.com>
2025-03-13 14:40:13 -0500

    created simpleCalculator component, connected to app

--- homework/20-angular-signals/20-homework/src/app/app.component.ts
+++ homework/20-angular-signals/20-homework/src/app/app.component.ts
@@ -4,13 +4,15 @@ import { CounterComponent } from "./counter/counter.component";
 import { TaskListComponent } from './task-list/task-list.component';
 import { FavColorComponent } from './fav-color/fav-color.component';
 import { TempConverterComponent } from './temp-converter/temp-converter.component';
+import { SimpleCalculatorComponent } from './simple-calculator/simple-calculator.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent, TaskListComponent, FavColorComponent, TempConverterComponent],
+  imports: [RouterOutlet, CounterComponent, TaskListComponent, FavColorComponent, TempConverterComponent, SimpleCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = '20-homework';
 }
+
---
+++ homework/20-angular-signals/20-homework/src/app/simple-calculator/simple-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-calculator',
+  imports: [],
+  templateUrl: './simple-calculator.component.html',
+  styleUrl: './simple-calculator.component.css'
+})
+export class SimpleCalculatorComponent {
+
+}
