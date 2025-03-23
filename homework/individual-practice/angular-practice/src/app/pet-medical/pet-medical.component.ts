595d70c238e72c699d60c4e0bb260aa7c7d95db7
rc-devs <rc.devs9@gmail.com>
2025-03-23 09:18:11 -0500

    create new pet-medical component

---
+++ homework/individual-practice/angular-practice/src/app/pet-medical/pet-medical.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-medical',
+  imports: [],
+  templateUrl: './pet-medical.component.html',
+  styleUrl: './pet-medical.component.css'
+})
+export class PetMedicalComponent {
+
+}
