b89fc79f7a429e2e11b584d5c4905f397e5004c8
rc-devs <rc.devs9@gmail.com>
2025-03-23 09:49:44 -0500

    create grocery list components and template, import and display component

--- homework/individual-practice/angular-practice/src/app/app.component.ts
+++ homework/individual-practice/angular-practice/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 
 //component imports
 import { PetMedicalComponent } from './pet-medical/pet-medical.component';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PetMedicalComponent],
+  imports: [PetMedicalComponent, GroceryListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ homework/individual-practice/angular-practice/src/app/grocery-list/grocery-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery-list',
+  imports: [],
+  templateUrl: './grocery-list.component.html',
+  styleUrl: './grocery-list.component.css'
+})
+export class GroceryListComponent {
+
+}
